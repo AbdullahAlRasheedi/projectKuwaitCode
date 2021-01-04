a7b898de0aa95421bf6d3946dc1a9e09032db960
AbdullahAlRasheedi <alkw444@gmail.com>
2021-01-04 21:04:14 +0300

    <V 1.6>
    add animation

--- search.js
+++ search.js
@@ -1,7 +1,27 @@
-function send(){
+function userIntery(){
     let search = document.getElementById("search").value;
-    document.getElementById("blink").href=`https://www.blink.com.kw/en/Product/Products?searchText=${search}`;
-    document.getElementById("amazon").href=`https://www.amazon.com/s?k=${search}`;
-    document.getElementById("quadra").href=`https://quadrastores.com/index.php/home/products/0/0/0/${search}`;
-    document.getElementById("newEgg").href=`https://www.newegg.com/global/kw-en/p/pl?d=${search}`;
+    return search;
+}
+function remove(){
+    document.getElementById("show").style.opacity = 0;
+    document.getElementById("show").style.transform = "translate(0,150px)";
+}
+
+function show(){
+    if(userIntery() == 0){
+        remove();
+    }
+    else{
+        document.getElementById("show").style.transform = "translate(0,-150px)";
+        document.getElementById("show").style.opacity = 1;
+    }
+}
+
+
+function send(){
+    show();
+    document.getElementById("blink").href=`https://www.blink.com.kw/en/Product/Products?searchText=${userIntery()}`;
+    document.getElementById("amazon").href=`https://www.amazon.com/s?k=${userIntery()}`;
+    document.getElementById("quadra").href=`https://quadrastores.com/index.php/home/products/0/0/0/${userIntery()}`;
+    document.getElementById("newEgg").href=`https://www.newegg.com/global/kw-en/p/pl?d=${userIntery()}`;
 }
